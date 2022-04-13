91463a1780db5a67a7ff643d2f7ee9ef0744f21e
Rajat Kumar <rajatkumardhingra27@gmail.com>
2022-04-13 14:07:42 +0530

    Android App

--- app/src/main/java/com/example/hike/MainActivity.java
+++ app/src/main/java/com/example/hike/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.hike;
 
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+//        Button b = (Button) findViewById(R.id.button);
+//        b.setOnClickListener(new onClickListner(){
+//
+//        });
     }
+
+    public void showText(View V){
+        System.out.println("ShowText");
+        System.out.println(findViewById(R.id.button));
+        TextView t =new TextView(getApplicationContext());
+        t.append("HIKE");
+        t.setGravity(Gravity.CENTER);
+        this.addContentView(t,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+    }
+
 }
